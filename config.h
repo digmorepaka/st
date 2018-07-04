e70075c91e8af2263330df7b19c78166ccfe592a
digmorepaka <danielkotondra@gmail.com>
2018-07-04 05:48:56 +0000

    Minor changes
    Minor changes

--- config.h
+++ config.h
@@ -113,8 +113,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 12;
-unsigned int defaultbg = 256;
+unsigned int defaultfg = 0;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 14;
 static unsigned int defaultrcs = 15;
 
